2eb8f892fd18c4989945470181aec00dfd78083b
KyTama <prtama95@gmail.com>
2026-06-29 03:18:00 +0700

    feat(01.2-06): assemble landing page in App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,15 @@
-import { LandingPage } from '@/pages'
+import { Hero } from './components/landing/Hero'
+import { Services } from './components/landing/Services'
+import { About } from './components/landing/About'
+import { Footer } from './components/landing/Footer'
 
 function App() {
   return (
-    <div className="text-red-500">
-      <LandingPage />
+    <div className="flex flex-col min-h-screen">
+      <Hero />
+      <Services />
+      <About />
+      <Footer />
     </div>
   )
 }
