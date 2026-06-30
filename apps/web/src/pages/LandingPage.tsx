4dd4ea5947931c12a29fee412d440fd4628a27cd
KyTama <prtama95@gmail.com>
2026-07-01 02:38:09 +0700

    feat(01.3.2): refactor LandingPage UI components to match Figma sequence

--- apps/web/src/pages/LandingPage.tsx
+++ apps/web/src/pages/LandingPage.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Navbar } from '../components/common/Navbar';
-import { Hero, WhyUs, Products, Psychologists, Insights, CtaScreening, Contact, Footer } from '../components/landing';
-
-const Testimonials = () => <section>Testimonials</section>;
+import { Hero } from '../components/landing/Hero';
+import { AboutUs } from '../components/landing/AboutUs';
+import { WhyAttentive } from '../components/landing/WhyAttentive';
+import { Psychologists } from '../components/landing/Psychologists';
+import { Testimonials } from '../components/landing/Testimonials';
+import { Services } from '../components/landing/Services';
+import { PartnerContent } from '../components/landing/PartnerContent';
+import { CtaScreening as CtaTakeFirstStep } from '../components/landing/CtaScreening';
+import { ClinicsLocations } from '../components/landing/ClinicsLocations';
+import { Footer } from '../components/landing/Footer';
 
 export const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <Hero />
-      <WhyUs />
-      <Products />
+      <AboutUs />
+      <WhyAttentive />
       <Psychologists />
-      <Insights />
       <Testimonials />
-      <CtaScreening />
-      <Contact />
+      <Services />
+      <PartnerContent />
+      <CtaTakeFirstStep />
+      <ClinicsLocations />
       <Footer />
     </div>
   );
